ea2a2e1601597cd4519bce33a9811fba9f95fcb2
Andrew <andrew@circa42.com>
2020-04-02 15:47:47 -0700

    Comments, bylines

---
+++ config/comments.php
@@ -0,0 +1,62 @@
+<?php
+
+return [
+    '*' => [
+        'indexSidebarLimit' => 25,
+        'indexSidebarGroup' => true,
+        'indexSidebarIndividualElements' => false,
+        'defaultQueryStatus' => ['approved'],
+
+        // General
+        'allowAnonymous' => false,
+        'guestRequireEmailName' => true,
+        'requireModeration' => true,
+        'moderatorUserGroup',
+        'autoCloseDays' => '',
+
+        // Voting
+        'allowVoting' => false,
+        'allowAnonymousVoting' => false,
+        'downvoteCommentLimit' => 5,
+        'hideVotingForThreshold' => false,
+
+        // Flagging
+        'allowFlagging' => false,
+        'allowAnonymousFlagging' => false,
+        'flaggedCommentLimit' => 5,
+
+        // Templates - Default
+        'showAvatar' => false,
+        'placeholderAvatar' => '',
+        'showTimeAgo' => true,
+        'outputDefaultCss' => true,
+        'outputDefaultJs' => true,
+
+        // Templates - Custom
+        'templateFolderOverride' => '',
+
+        // Security
+        'enableSpamChecks' => true,
+        'securityMaxLength' => '',
+        'securityFlooding' => '',
+        'securityModeration' => '',
+        'securityBlacklist' => '',
+        'securityBanned' => '',
+        'recaptchaEnabled' => false,
+        'recaptchaKey' => '',
+        'recaptchaSecret' => '',
+
+        // Notifications
+        'notificationAuthorEnabled' => true,
+        'notificationReplyEnabled' => true,
+        'notificationSubscribeAuto' => false,
+        'notificationSubscribeDefault' => true,
+        'notificationSubscribeEnabled' => false,
+        'notificationSubscribeCommentEnabled' => false,
+        'notificationModeratorEnabled' => false,
+        'notificationModeratorApprovedEnabled' => false,
+
+        // Permissions
+        'permissions' => [],
+    ]
+];
